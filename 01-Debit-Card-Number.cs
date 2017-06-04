ffc8c0e910b003fe3daffcbc24e988e84076f58f
kriss-ivnff <super_gravity@abv.bg>
2017-06-04 19:18:14 +0300

    Add files via upload

---
+++ 01-Debit-Card-Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01_Debit_Card_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3= int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(String.Format("{0:0000} {1:0000} {2:0000} {3:0000}", num1 , num2, num3, num4));
+        }
+    }
+}
